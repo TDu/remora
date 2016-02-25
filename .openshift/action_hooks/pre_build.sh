38ba329311b986f90a8bca5ed5680e25da72f183
Thierry Ducrest <thierry@ducrest.net>
2016-02-25 15:05:43 -0300

    Add hook to fix libxmljs bug before build

---
+++ .openshift/action_hooks/pre_build.sh
@@ -0,0 +1,3 @@
+sed -i '/product_extension/d' ~/app-root/runtime/repo/node_modules/node-rss/node_modules/libxmljs/binding.gyp
+
+sed -i '/shared_library/d' ~/app-root/runtime/repo/node_modules/node-rss/node_modules/libxmljs/binding.gyp
