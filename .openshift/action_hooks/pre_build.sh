0d33c25cf5f33aecb4935440ecfe6c2ddce62ddc
Thierry Ducrest <thierry@ducrest.net>
2016-02-25 15:19:16 -0300

    Fix that build

--- .openshift/action_hooks/pre_build.sh
+++ .openshift/action_hooks/pre_build.sh
@@ -1,3 +1,6 @@
 sed -i '/product_extension/d' ~/app-root/runtime/repo/node_modules/node-rss/node_modules/libxmljs/binding.gyp
 
 sed -i '/shared_library/d' ~/app-root/runtime/repo/node_modules/node-rss/node_modules/libxmljs/binding.gyp
+
+cd ~/app-root/runtime/repo/node_modules/node-rss/node_modules/libxmljs
+node-gyp rebuild
