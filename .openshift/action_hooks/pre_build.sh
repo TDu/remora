4341b0fd2bd8bcca2e07a95b10ed18e7465db0f6
Thierry Ducrest <thierry@ducrest.net>
2016-02-25 20:04:49 -0300

    One more fix for libxml

--- .openshift/action_hooks/pre_build.sh
+++ .openshift/action_hooks/pre_build.sh
@@ -2,5 +2,6 @@ sed -i '/product_extension/d' ~/app-root/runtime/repo/node_modules/node-rss/node
 
 sed -i '/shared_library/d' ~/app-root/runtime/repo/node_modules/node-rss/node_modules/libxmljs/binding.gyp
 
+sed -i '375 c#if 0' 
 cd ~/app-root/runtime/repo/node_modules/node-rss/node_modules/libxmljs
 node-gyp rebuild
