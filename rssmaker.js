8c0f523c50d3cee8b16bb5227e84d80febbfbda5
Thierry Ducrest <thierry@ducrest.net>
2016-02-25 21:18:31 -0300

    Change package for rss feed creation

--- rssmaker.js
+++ rssmaker.js
@@ -1,32 +1,36 @@
 //
 var cheerio = require('cheerio');
-var rss = require('node-rss');
+var RSS = require('rss');
 
 var createFeed = function createFeed (html, url ) {
 
     //Utilize cheerio to get jquery functionality
     var $ = cheerio.load(html);
     //Create the new feed
-    var feed = rss.createNewFeed('Blog Most Recent',
-            'http://www.beachgrit.com/',
-            'Most recent blog entries from beachgrit',
-            '', //author
-            'http://someurl.com/rss/MostRecent.xml'
-            );
-    //Get the feed entries
-    $('#ajax-filtered-section').filter(function () {
-        $(this).find('article').each(function (index, value){
-            var title,
-            url,
-            description;
-            title = $(this).find('h3').text();
-            url = $(this).find('a').attr('href');
-            description = $(this).find('p').text();
-            feed.addNewItem(title, url, new Date(), description, {});
-
-        });
+    //var feed = rss.createNewFeed('Blog Most Recent',
+            //'http://www.beachgrit.com/',
+            //'Most recent blog entries from beachgrit',
+            //'', //author
+            //'http://someurl.com/rss/MostRecent.xml'
+            //);
+    var feed= new RSS({
+        title: 'Beachgrit rss'
     });
-    var xmlString = rss.getFeedXML(feed);
+
+    //Get the feed entries
+//    $('#ajax-filtered-section').filter(function () {
+//        $(this).find('article').each(function (index, value){
+//            var title,
+//            url,
+//            description;
+//            title = $(this).find('h3').text();
+//            url = $(this).find('a').attr('href');
+//            description = $(this).find('p').text();
+//            feed.addNewItem(title, url, new Date(), description, {});
+//
+//        });
+//    });
+    var xmlString = feed.xml();//rss.getFeedXML(feed);
     return xmlString;
 };
 
