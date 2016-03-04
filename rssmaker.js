436927591681934174a69825d7b428913a368ac7
Thierry Ducrest <thierry@ducrest.net>
2016-03-03 22:16:47 -0300

    Clean up

--- rssmaker.js
+++ rssmaker.js
@@ -1,9 +1,8 @@
-//Make an rss feed for the source code of a web page
+//Make an rss feed from the source code of a web page
 var cheerio = require('cheerio');
 var RSS = require('rss');
 
 var createFeed = function createFeed (html, feed_param, params) {
-
     //Utilize cheerio to get jquery functionality
     var $ = cheerio.load(html);
     //Create the new feed
@@ -12,16 +11,27 @@ var createFeed = function createFeed (html, feed_param, params) {
     $(params.argument_items_section).filter(function () {
         $(this).find(params.argument_item).each(function (index, value){
             var title,
+                site_url,
                 url,
-                description;
+                description,
+                rootUrl;
+
             title = $(this).find(params.argument_item_title).text();
             url = $(this).find('a').attr('href');
-            //Change relative links into absolute if needed
-            if (url.indexOf('//') == -1) {
-                url = feed_param.site_url + url;
-            }
             description = $(this).find(params.argument_item_description).text();
-            console.log(title, url);
+            //Change relative links into absolute if needed
+            site_url = feed_param.site_url;
+            if (url.indexOf('//') == -1) {
+                //get the root url from the site url
+                if (feed_param.site_url.indexOf('://') > -1) {
+                    rootUrl = site_url.substr(0,site_url.indexOf('/', 8));
+                } else {
+                    rootUrl = site_url.substr(0,site_url.indexOf('/', 1));
+                }
+                url = rootUrl + url;
+            }
+            //console.log(title, url);
+            //Add the new entry to the feed
             feed.item({
                 title: title,
                 url: url,
@@ -30,8 +40,8 @@ var createFeed = function createFeed (html, feed_param, params) {
             });
         });
     });
-    var xmlString = feed.xml();
-    return xmlString;
+    //Create the feed
+    return feed.xml();
 };
 
 module.exports = createFeed;
