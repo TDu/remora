bd7ee35edc39f0dc6748bd8375b6a98b34e5b8e7
Thierry Ducrest <thierry@ducrest.net>
2016-03-08 00:44:45 -0300

    Add the list of feed generated in the front page

---
+++ static/main.js
@@ -0,0 +1,27 @@
+(function () {
+    console.log("Javascript loaded.");
+    document.addEventListener('DOMContentLoaded', function () {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                console.log('Received the payload : ');
+                console.log(xmlHttp.responseText);
+                //Display the list of rss feed to the user
+                var feedlist = JSON.parse(xmlHttp.responseText);
+                var ul = document.getElementById('feedlist');
+                for (var feed in feedlist) {
+                    var li = document.createElement("li");
+                    var a = document.createElement("a");
+                    a.setAttribute('href', feedlist[feed]);
+                    a.innerHTML = feed;
+                    //li.appendChild(document.createTextNode(feed));
+                    li.appendChild(a);
+                    ul.appendChild(li);
+                }
+            }
+        }
+        xmlHttp.open("GET", "/feedlist", true)
+            xmlHttp.send(null);
+    }, false);
+    //Load the list of rss feed generated
+})();
